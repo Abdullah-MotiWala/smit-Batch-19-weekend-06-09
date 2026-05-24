8f978b3592bb209e5b6a72142c8e2f805ae214db
hasanCoder66 <hasanashraf874@gmail.com>
2026-05-24 20:42:11 +0500

    backend crud covered

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,47 @@
 const http = require("http");
+
+const users = [{ id: 1, name: "Ali", age: 24 }];
+let lastAddedID = 1;
+
 const server = http.createServer((req, res) => {
-  console.log(req.method, req.url);
-  if (req.url === "/signup" && req.method === "POST") {
-    // Database connection
-    res.write("Signed up");
+  if (req.url === "/user" && req.method === "POST") {
+    req.on("data", (chunk) => {
+      const result = chunk;
+      const body = JSON.parse(result);
+      users.push({ ...body, id: ++lastAddedID });
+      res.write(JSON.stringify({ id: lastAddedID }));
+      res.end();
+    });
+  }
+  if (req.url.includes("/user") && req.method === "GET") {
+    const id = req.url.split("?")[1].split("=")[1];
+    const user = users.find((u) => u.id == id);
+    if (user) {
+      res.write(JSON.stringify(user));
+    }
     res.end();
   }
-  if (req.url === "/signin" && req.method === "GET") {
-    // Database connection
-    res.write("Login");
+  if (req.url.includes("/user") && req.method === "DELETE") {
+    const id = req.url.split("?")[1].split("=")[1];
+    const userIndex = users.findIndex((u) => u.id == id);
+    users.splice(userIndex, 1);
+    res.write("user deleted");
     res.end();
   }
-  
-  if (req.url === "/username" && req.method === "GET") {
-    // Database connection
-    res.write("abdullah");
-    res.end();
+  if (req.url.includes("/user") && req.method === "PUT"){
+    const id = req.url.split("?")[1].split("=")[1];
+    const userIndex = users.findIndex((u) => u.id == id);
+    req.on("data", (chunk) => {
+      const result = chunk;
+      const body = JSON.parse(result);
+      users[userIndex] = {...body, id}
+      res.write(JSON.stringify({ id: lastAddedID }));
+      res.end();
+    })
   }
 });
 const PORT = 5000;
 
 server.listen(PORT, () => {
-  console.log("Server is running on 5000");
+  console.log("Server is listening on 5000");
 });
-
